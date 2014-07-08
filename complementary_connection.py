6d6eca4b5d876ee19daeb1850955804f71a06cb4
arknoah <surfer197@gmail.com>
2014-07-08 14:32:43 +0800

    done complementary part

--- complementary_connection.py
+++ complementary_connection.py
@@ -14,7 +14,7 @@ def checkTemperaments(list1,list2):
 def get_complementary(std_id):
     # populate the list of user to be compare with
     compare_list = app_datastore.get_other_records()
-    symmetrical_list = {}
+    complementary_list = {}
     index=0
     num_of_matching_asp = 0
     total_num_of_compare_std_asp = 0
@@ -54,28 +54,30 @@ def get_complementary(std_id):
                     formula = float((float(num_of_matching_asp / total_num_of_compare_std_asp) + float(
                         num_of_not_matching_personality / 40.0)) / 2.0) * 100.0
                     result = round(formula, 1)
-                    symmetrical_list[other_user.student_id] = result
+                    complementary_list[other_user.student_id] = result
                 except Exception:
                     pass
         except Exception:
             pass
-    sorted_dict=sorted(symmetrical_list,key=symmetrical_list.get, reverse=True)
+    sorted_dict=sorted(complementary_list,key=complementary_list.get, reverse=True)
     result_dict={}
+    place_index=0
     for x in xrange(0,len(sorted_dict)):
-        p_percent='person_'+str(index)+'_percent'
-        p_name='person_'+str(index)+'_name'
-        p_dob='person_'+str(index)+'_dob'
-        p_country='person_'+str(index)+'_country'
-        p_major='person_'+str(index)+'_major'
-        p_faculty='person_'+str(index)+'_faculty'
-        p_skills='person_'+str(index)+'_skills'
-        p_interests='person_'+str(index)+'_interests'
-        p_involvements='person_'+str(index)+'_involvements'
-        p_module='person_'+str(index)+'_best_modules'
-        p_advice='person_'+str(index)+'_advice'
-        p_aspirations='person_'+str(index)+'_aspirations'
-        p_networks='person_'+str(index)+'_networks'
-        p_website='person_'+str(index)+'_website'
+        place_index=x+1
+        p_percent='person_'+str(place_index)+'_percent'
+        p_name='person_'+str(place_index)+'_name'
+        p_dob='person_'+str(place_index)+'_dob'
+        p_country='person_'+str(place_index)+'_country'
+        p_major='person_'+str(place_index)+'_major'
+        p_faculty='person_'+str(place_index)+'_faculty'
+        p_skills='person_'+str(place_index)+'_skills'
+        p_interests='person_'+str(place_index)+'_interests'
+        p_involvements='person_'+str(place_index)+'_involvements'
+        p_module='person_'+str(place_index)+'_best_modules'
+        p_advice='person_'+str(place_index)+'_advice'
+        p_aspirations='person_'+str(place_index)+'_aspirations'
+        p_networks='person_'+str(place_index)+'_networks'
+        p_website='person_'+str(place_index)+'_website'
         result_dict[p_percent]=int(round(symmetrical_list.get(sorted_dict[index])))
         result_dict[p_name]=app_datastore.get_user(sorted_dict[index]).name
         result_dict[p_dob]=app_datastore.get_user(sorted_dict[index]).date_of_birth
